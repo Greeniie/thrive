14bb75ade62bed6149cc6aef7811b1c642aae6b9
Tejiri Anthony <tejirianthony07@gmail.com>
2023-04-14 11:37:00 +0100

    ...

--- src/components/pages/About/CTA2.js
+++ src/components/pages/About/CTA2.js
@@ -5,7 +5,7 @@ const CTA2 = () => {
     <div className="min-h-[85vh] hero-bg2 mt-10">
       <div className="w-4/5 mx-auto pt-10 md:pt-28">
         <div className="t-textbig">One-time sign-up.</div>
-        <div className="t-textbig"> Easy sign-ins.</div>
+        <div className="t-textbig2"> Easy sign-ins.</div>
 
 
         <div className="t-textsmall w-full md:w-1/2 text-white leading-snug">
@@ -15,11 +15,11 @@ const CTA2 = () => {
           fraction of the actual cost. Thanks to support from our Co-Sponsors
           led by GIZ/GOPA.
         </div>
-        <div className="flex flex-col md:flex-row gap-2">
+        <div className="flex flex-col md:flex-row gap-2 pb-3 md:pb-0">
           <button className="bg-[#ff0000] text-white rounded-sm py-3 px-5">
             Become a Sponsor
           </button>
-          <button className="bg-white text-black rounded-sm py-3 px-5 mb-3 md:mb-0">
+          <button className="bg-white text-black rounded-sm py-3 px-5 ">
             I am a Business Owner
           </button>
         </div>
