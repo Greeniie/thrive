51c205420ba6127be4073306276d235449434dc7
Tejiri Anthony <tejirianthony07@gmail.com>
2023-04-15 09:35:16 +0100

    Added new consortium members

--- src/components/Landing/Clients.js
+++ src/components/Landing/Clients.js
@@ -1,8 +1,16 @@
 import React from "react";
 import enter from "../assets/enterprise.jpeg";
-import value from "../assets/valuecon.jpeg";
+import value from "../assets/valuecon.png";
 import woh from "../assets/woh.jpeg";
 import hope from "../assets/hope.png";
+import viis from "../assets/viis.png";
+import lapo from "../assets/lapo.jpg";
+import sabi from "../assets/sabi.png";
+import dofoll from "../assets/dofoll.png";
+import seyp from "../assets/seyp.png";
+import kairos from "../assets/kairos.jpg";
+
+
 
 const Clients = () => {
   return (
@@ -12,11 +20,11 @@ const Clients = () => {
         <h2 className="text-[50px] mb-5 text-[#ff0000] hero">
           Meet Our Consortium Members
         </h2>
-        <div className="block md:grid md:overflow-hidden md:grid-cols-4 md:grid-rows-1 gap-2">
+        <div className="block md:grid md:overflow-hidden md:grid-cols-4 md:grid-rows-1 gap-2 md:items-center">
           <a href="http://www.enterprisehubs.com/" target="_blank" rel="noopener noreferrer">
-            <div className="flex items-center justify-center md:border border-x border-t-0 hover:border-b-[#ff0000] hover:border-b-[4px] hover:shadow-lg">
+            <div>
               <img
-                className="min-w-full object-cover h-auto p-20 row-span-1 col-span-1"
+                className="min-w-full object-contain h-auto p-5 row-span-1 col-span-1"
                 src={enter}
                 alt="icon"
               />
@@ -24,9 +32,9 @@ const Clients = () => {
           </a>
 
           <a href="https://valuecon.com.ng" target="_blank" rel="noopener noreferrer">
-            <div className="flex items-center justify-center md:border border-x border-t-0 hover:border-b-[#ff0000] hover:border-b-[4px] hover:shadow-lg">
+            <div>
               <img
-                className="min-w-full object-cover h-auto p-20 row-span-1 col-span-1"
+                className="min-w-full object-contain h-auto p-5 row-span-1 col-span-1"
                 src={value}
                 alt="icon"
               />
@@ -34,23 +42,74 @@ const Clients = () => {
           </a>
 
           <a href="https://webofheartsfoundation.org" target="_blank" rel="noopener noreferrer">
-            <div className="flex items-center justify-center md:border border-x border-t-0 hover:border-b-[#ff0000] hover:border-b-[4px] hover:shadow-lg">
+            <div>
               <img
-                className="min-w-full object-cover h-auto p-20 row-span-1 col-span-1"
+                className="min-w-full object-contain h-auto p-5 row-span-1 col-span-1"
                 src={woh}
                 alt="icon"
               />
             </div>
           </a>
           <a href="https://hopebuildersconsult.org" target="_blank" rel="noopener noreferrer">
-            <div className="flex items-center justify-center md:border border-x border-t-0 hover:border-b-[#ff0000] hover:border-b-[4px] hover:shadow-lg">
+            <div>
               <img
-                className="w-full object-cover object-center h-[316px] py-[120px] row-span-1 col-span-1"
+                                className="min-w-full object-contain h-auto p-5 row-span-1 col-span-1"
+
                 src={hope}
                 alt="icon"
               />
             </div>
           </a>
+          <a href="https://viisaus.com" target="_blank" rel="noopener noreferrer">
+            <div>
+              <img
+                className="min-w-full object-contain h-auto p-5 row-span-1 col-span-1"
+                src={viis}
+                alt="icon"
+              />
+            </div>
+          </a> <a href="https://lapoinstitute.edu.ng" target="_blank" rel="noopener noreferrer">
+            <div>
+              <img
+                className="min-w-full object-contain h-auto p-5 row-span-1 col-span-1"
+                src={lapo}
+                alt="icon"
+              />
+            </div>
+          </a> <a href="https://sabihub.org" target="_blank" rel="noopener noreferrer">
+            <div>
+              <img
+                className="min-w-full object-contain h-auto p-5 row-span-1 col-span-1"
+                src={sabi}
+                alt="icon"
+              />
+            </div>
+          </a> <a href="https://dofoll.com" target="_blank" rel="noopener noreferrer">
+            <div>
+              <img
+                className="min-w-full object-contain h-auto p-5 row-span-1 col-span-1"
+                src={dofoll}
+                alt="icon"
+              />
+            </div>
+          </a> <a href="https://seypng.org" target="_blank" rel="noopener noreferrer">
+            <div>
+              <img
+                className="min-w-full object-contain h-auto p-5 row-span-1 col-span-1"
+                src={seyp}
+                alt="icon"
+              />
+            </div>
+          </a>
+
+            <div>
+              <img
+                className="min-w-full object-contain h-auto p-5 row-span-1 col-span-1"
+                src={kairos}
+                alt="icon"
+              />
+            </div>
+          
         </div>
       </section>
     </div>
--- src/components/Landing/Sponsors.js
+++ src/components/Landing/Sponsors.js
@@ -1,7 +1,6 @@
 import React from "react";
 import giz from "../assets/giz-logo.gif";
 import gopa from "../assets/gopa.png";
-import rigo from "../assets/rigo-mfb.jpeg";
 
 
 const Sponsors = () => {
@@ -11,7 +10,7 @@ const Sponsors = () => {
       <section className="text-center flex flex-col items-center mb-20 mt-[100px] w-[90%] mx-auto">
         <h2 className="text-[50px] mb-5 text-[#ff0000] hero">Our Sponsors</h2>
 
-        <div className="block md:grid md:overflow-hidden md:grid-cols-3 md:grid-rows-1 gap-2">
+        <div className="block md:grid md:overflow-hidden md:grid-cols-2 md:grid-rows-1 gap-2">
           <div className="h-[100%] row-span-1 col-span-1">
             <a href="http://giz.de" target="_blank" rel="noopener noreferrer">
               <div className="flex items-center justify-center md:border border-x border-t-0 hover:border-b-[#ff0000] hover:border-b-[4px] hover:shadow-lg">
@@ -35,17 +34,7 @@ const Sponsors = () => {
               </div>
             </a>
           </div>
-          <div className="h-auto  row-span-1 col-span-1">
-            <a href="https://rigomfb.com" target="_blank" rel="noopener noreferrer">
-              <div className="flex items-center justify-center md:border border-x border-t-0 hover:border-b-[#ff0000] hover:border-b-[4px] hover:shadow-lg">
-                <img
-                  className="min-w-full object-cover p-10"
-                  src={rigo}
-                  alt="icon"
-                />
-              </div>
-            </a>
-          </div>
+
         </div>
       </section>
     </div>
